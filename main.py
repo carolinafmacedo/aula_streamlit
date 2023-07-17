5b421c646f0df32fc6ee455ebd0944e1c1e7e50b
carolina <107003191+carolinafmacedo@users.noreply.github.com>
2023-07-17 08:26:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,18 @@ fig.add_trace(go.Scatter(x = data.index, y = data['Close'], name = 'Fechamento')
 fig.update_layout(title = f"{ticker_symbol}", xaxis_title = 'Data', yaxis_title = 'Preço')
 st.plotly_chart(fig)
 
-# Exibir o gráfico de volume
-fig_volume = go.Figure()
-fig_volume.add_trace(go.Scatter(x=data.index, y=data['Volume'], name='Volume'))
-fig_volume.update_layout(title=f"{ticker_symbol}", xaxis_title='Data', yaxis_title='Volume')
-st.plotly_chart(fig_volume)
+st.sidebar.title('Selecione o stock - Grafico 2')
+ticker_symbol2 = st.sidebar.text_input('stock', 'MSFT', max_chars=10)
+
+# baixando dados do yahoo
+data2 = yf.download(ticker_symbol2, start = '2020-01-01', end = '2023-06-26')
+
+# exibir os dados
+st.subheader('Histórico')
+st.dataframe(data)
+
+# Exibir o gráfico
+fig_close2 = go.Figure()
+fig_close2.add_trace(go.Scatter(x = data2.index, y = data2['Close'], name = 'Fechamento'))
+fig_close2.update_layout(title = f"{ticker_symbol2}", xaxis_title = 'Data', yaxis_title = 'Preço')
+st.plotly_chart(fig_close2)
